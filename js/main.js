1a7e333e8c15a8fdb2a39dcb30a7d32bf93452a0
metoxys <metoxys@gmx.net>
2016-03-23 22:39:27 +0100

    Activating rain properly, take 2

--- js/main.js
+++ js/main.js
@@ -102,6 +102,7 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
   $scope.illionsArray = illionsArr.slice(1);
   $scope.mars = {};
   $scope.moon = {};
+  $scope.rain = {};
   $scope.raw = false;
   $scope.ref = $scope.earth;
   $scope.reverse = true;
@@ -881,6 +882,10 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
     return $scope.ref === $scope.moon;
   };
   
+  $scope.isRain = function() {
+    return $scope.ref === $scope.rain;
+  };
+  
   function lzf_decode(str) {
     var iidx = 0, oidx = 0, oLen = str.length,
     temp = Array.apply(null, new Array(oLen)).map(Number.prototype.valueOf, 0);
@@ -1026,6 +1031,10 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
   $scope.setMoon = function() {
     setPlanet('moon')
   };
+  
+  $scope.setRain = function() {
+    setPlanet('rain')
+  }
 
   function setPlanet(planet) {
     $scope.clearAfter = [false, false];
