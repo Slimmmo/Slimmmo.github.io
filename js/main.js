f1cdfc4392823a2a2a3e30596135e1cf0d984cd1
dr-ishmael <dr.ishmael@gmail.com>
2016-08-10 13:39:18 -0500

    fix power of Larping

--- js/main.js
+++ js/main.js
@@ -1353,7 +1353,7 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
   function loadDefaults() {
     $scope.lyp.angelScale = 1; //???
     $scope.lyp.baseCost = [5, 88, 777, 22222, 444444, 99999999, 222222222, 5555555555, 33333333333];
-    $scope.lyp.basePower = [1.023, 1.045, 1.083, 1.166, 1, 1, 1, 1, 1];
+    $scope.lyp.basePower = [1.023, 1.046, 1.083, 1.166, 1, 1, 1, 1, 1];
     $scope.lyp.baseProfit = [0.5, 9, 81, 729, 1, 1, 1, 1, 1];
     $scope.lyp.baseSpeed = [1.5, 5, 8, 18, 1, 1, 1, 1, 1];
     $scope.lyp.hasMegaTickets = false;
