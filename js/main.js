cacf12841c689c73f708dce0c30577e761d03a42
Ricardo Petrére <ricardo19.93@hotmail.com>
2016-09-03 06:06:02 -0300

    Excellent - Super Badge (#107)
    * New Super Badge
    
    * Typo

--- js/main.js
+++ js/main.js
@@ -136,7 +136,8 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
     ['Space Buddies', 1, [8, 20]],
     ['Silver Blade', 1, [12, 20]],
     ['Speaker', 1, [16, 15]],
-    ['Boxing Bear', 2, [8, 20]]
+    ['Boxing Bear', 2, [8, 20]],
+    ['Time Machine', 2, [14, 15]]
   ];
   var planets = ['earth', 'moon', 'mars', 'excellent'];
 
