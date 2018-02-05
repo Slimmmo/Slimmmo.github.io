7d51218c1bfcc607c9b4c6d465bdb912250b6a34
Nutty69 <36167561+Nutty69@users.noreply.github.com>
2018-02-05 15:19:03 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1805,8 +1805,8 @@ advApp.controller('advController', ['$document', '$filter', '$scope', function($
       ['Budget Lite', 0, false, 0, 0, 0, 0],
       ['Foam Stiff-arms', 0, false, 0, 0, 0, 0],
       ['Skybox Seating', 0, false, 0, 0, 0, 0],
-      ['Designer Uniforms', 0, false, 0, 0, 0, 0],
       ['Hypno-ads', 0, false, 0, 0, 0, 0],
+      ['Designer Uniforms', 0, false, 0, 0, 0, 0],
     ];
     $scope.rain.angelScale = 45;
     $scope.rain.baseCost = [7, 83, 749, 24195, 402667, 98285656, 174429216, 5758901760, 33333333333];
