4cf080b690e06d05328d70dc01b44edecbd21503
Slimmmo <danielksimpson@gmail.com>
2024-02-09 21:29:44 +1100

    Undo update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
 	 *
 	 * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
 	 */
-	basePath: "slimmmo.github.io",
+	basePath: "",
   
 	/**
 	 * Disable server-based image optimization. Next.js does not support
