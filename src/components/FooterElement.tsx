0f6db379e8fc77f4741d21c46c442dd7245e336a
Slimmmo <danielksimpson@gmail.com>
2024-02-10 16:05:54 +1100

    Made footer full width

--- src/components/FooterElement.tsx
+++ src/components/FooterElement.tsx
@@ -2,7 +2,7 @@ import { BsGithub, BsReddit } from 'react-icons/bs'
 
 export default function FooterElement() {
 	return (
-		<div className='container w-full p-4 flex justify-center bg-secondary'>
+		<div className='w-full p-4 flex justify-center bg-secondary'>
 			<div className="mt-1 flex space-x-6 sm:mt-0 sm:justify-center">
 				<a href="https://www.reddit.com/r/AdventureCapitalist/comments/59knd5/gates_of_heck_calculator_updated/"><BsReddit /></a>
 				<a href="https://github.com/Slimmmo/Slimmmo.github.io"><BsGithub /></a>
